3b2458e185b38d0ad2410b98784eb06bcc4aeabb
sojibseikh <154545886+sojibseikh@users.noreply.github.com>
2024-01-02 11:56:36 +0600

    add index.php

---
+++ index.php
@@ -0,0 +1,147 @@
+<?php
+include_once 'config/database.php';
+
+include('header.php');
+
+
+
+
+
+
+?>
+
+<!-- home section start -->
+<section class="home" id="home">
+    <div class="max-width1">
+        <div class="home-content" id="h1"><br><br> <br><br><br><br><br><br><br><br>
+            <div class="text-1">Hello, my name is</div><br>
+            <div class="text-2">Sk Sojib</div>
+            <div class="text-3">And I'm a <span class="typing"></span></div><br><br><br><br>
+            <a href="feedback.php" id="text-4">Hire me</a>&#160 &#160 &#160 &#160 &#160 &#160 &#160
+            <a href="servicse.php" id="text-05">Lern More</a>
+        </div>
+    </div>
+</section>
+
+<!-- about section start -->
+<span style="display:none;" class="typing-2"></span>
+
+
+
+
+
+
+
+<!-- services section start -->
+<section class="services" id="services">
+    <div class="max-width">
+        <h2 class="title">My services</h2>
+        <div class="serv-content">
+            <div class="card">
+                <div class="box">
+                    <i class="fas fa-paint-brush"></i>
+                    <div class="text">Web Design</div>
+                    <p>Iam a Professional Web Desginar and Developer. Iam alwys work srious, i will try alwyas best of my skills. My design is perfect for every type website. </p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+                    <i class="fas fa-code"></i>
+                    <div class="text">Web Development</div>
+                    <p>Iam a Professional Web Desginar and Developer. I have created all type website like: protfolio, e_comerce etc.I have know all tag in html. My dvelopment skill </p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="box">
+
+                    <i class="fas fa-chart-line"></i>
+                    <div class="text">Database</div>
+                    <p>Iam also Professional of Database like: Wordpress, Php, Python, JavaScript Etc. I also skiled in mysql &amp;sql.My error heandaling skill good. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+<!-- skills section start sojib -->
+<section class="skills" id="skills">
+    <div class="max-width">
+        <h2 class="title">My skills</h2>
+        <div class="skills-content">
+            <div class="column left">
+
+                <div class="text">My creative skills experiences.</div>
+                <a href="#">Read more</a><br><br>
+                <p> I have finished a long training from Outsourcing Institute on total Web Devlopment Marketing and vary practical and many practical project. There I have gathered a lost of experience. Able to make effective and attractive web content to promote any product or business to million of people .I take them very seriously .I always work with focus. .Thanks.</p>
+
+            </div>
+            <div class="column right">
+                <div class="bars">
+                    <div class="info">
+                        <span>HTML</span>
+                        <span>90%</span>
+                    </div>
+                    <div class="line html"></div>
+                </div>
+                <div class="bars">
+                    <div class="info">
+                        <span>CSS</span>
+                        <span>90%</span>
+                    </div>
+                    <div class="line css"></div>
+                </div>
+                <div class="bars">
+                    <div class="info">
+                        <span>JavaScript</span>
+                        <span>70%</span>
+                    </div>
+                    <div class="line js"></div>
+                </div>
+                <div class="bars">
+                    <div class="info">
+                        <span>PHP</span>
+                        <span>90%</span>
+                    </div>
+                    <div class="line php"></div>
+                </div>
+                <div class="bars">
+                    <div class="info">
+                        <span>MySQL</span>
+                        <span>89%</span>
+                    </div>
+                    <div class="line mysql"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- teams section start -->
+<div class="container-fulid">
+    <div class="row">
+
+        <div class="col-md-offset-2 col-md-10">
+
+            <div id="div1">
+                <a href="contect.php">
+                    <div class="text-center">
+                        <h3 id="nam">Owner</h3>
+                    </div>
+                    <div id="raduice">
+                        <a href="about.php"> <img class="img-circle" src="images/sk_sojib1.jpg" width="200px;" style="border:1px solid white;"></a>
+                    </div>
+                    <p>My Protfilio Website</p>
+                </a>
+            </div>
+        </div>
+
+    </div>
+</div>
+<br><br>
+
+<?php include('end.php'); ?>
